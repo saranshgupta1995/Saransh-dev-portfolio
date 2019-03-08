bea02443034f891282e8d8ed0ce29f98b50470a3
Saransh Gupta <saranshgupta1995@gmail.com>
2019-03-08 09:08:16 +0530

    added coin sprite and spriter

--- portfolio-app/src/CanvasBG.js
+++ portfolio-app/src/CanvasBG.js
@@ -1,37 +1,68 @@
 import React, { Component } from 'react';
+import coinSheet from './coin-sheet.png';
+
+class Spriter{
+
+    constructor(spriteSheet,spriteW, spriteH, spriteStartX, spriteStartY, frameCount){
+        this.spriteSheet = spriteSheet;
+        this.spriteIndex=0;
+        this.spriteW = spriteW;
+        this.spriteH = spriteH;
+        this.spriteStartX = spriteStartX;
+        this.spriteStartY = spriteStartY;
+        this.frameCount=frameCount;
+    }
+
+    next(context, frameX, frameY, frameW, frameH){
+        context.clearRect(frameX, frameY, frameW, frameH)
+        context.drawImage(this.spriteSheet, this.spriteStartX + this.spriteIndex*this.spriteW, this.spriteStartY,this.spriteW, this.spriteH, frameX, frameY, frameW, frameH)
+        this.spriteIndex+=1;
+        if(this.spriteIndex===this.frameCount){
+            this.spriteIndex=0;
+        }
+    }
+}
 
 class CanvasBG extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
     }
 
-    componentDidMount(){
+    bgColor = 46;
+
+    componentDidMount() {
         let canvas = document.getElementById('cnv_bg');
         let ctx = canvas.getContext('2d');
 
-        var [w, h] = [canvas.width, canvas.height];
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
 
-        let ctxImage=ctx.createImageData(w,h);
-        let ctxImageData=ctxImage.data;
+        let allParticles = [];
 
-        class Particle {
-            x = 0;
-            y = 0;
-            opacity = 1;
+        let [w, h] = [canvas.width, canvas.height];
+
+        let coinImage = new Image();
+        coinImage.src = coinSheet;
+        coinImage.onload=()=>{
+          initCoinAnimation();  
         }
 
-        // hsla(208, 48 %, 40 %, 0.57)
+        let coinSprite = new Spriter(coinImage, 100, 100, 0, 0, 10);
+        let initCoinAnimation = () =>{
+            coinSprite.next(ctx, 0, 0, 100, 100);
+            requestAnimationFrame(initCoinAnimation)
+        }
 
     }
 
-  render() {
-    return (
-      <>
-      <canvas id="cnv_bg" style={{width:"100vw", height:"100vh", position:"absolute", zIndex:-999, top:0, left:0}}></canvas>
-      </>
-    );
-  }
+    render() {
+        return (
+            <>
+                <canvas id="cnv_bg" style={{ position: "absolute", zIndex: -999, top: 0, left: 0 }}></canvas>
+            </>
+        );
+    }
 }
 
 export default CanvasBG;
